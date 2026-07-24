d8c55196f739da48a2643cdcb68c52ecbca84f2b
Carl <carldjapardi7@gmail.com>
2026-07-24 16:29:51 -0700

    Add: find-target-in-rotated-sorted-array - submission-17

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-17.py
@@ -0,0 +1,18 @@
+class Solution:
+    # intuition for one pass: in which range is it sorted?
+    # intuition for two pass: get minimum's index, do bin search on two ranges
+    def search(self, nums, target): 
+        l = 0 
+        r = len(nums)-1
+        while l <= r:
+            m = (l+r)//2
+            if nums[m] == target:
+                return m
+            elif nums[m] <= nums[r]: # m<->r sorted
+                if nums[m] < target <= nums[r]: l = m+1
+                else: r = m-1
+            elif nums[m] > nums[r]: # l<->m sorted
+                if nums[l] <= target < nums[m]: r = m-1
+                else: l = m+1
+        return -1 
+
